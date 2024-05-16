0017b918fcd8370163d6e6110796a5b23c7230e5
sourav <sdsouravg1580@gmail.com>
2024-05-16 16:39:31 +0600

    added new user model

--- multivendor_food/settings.py
+++ multivendor_food/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -91,6 +92,8 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
